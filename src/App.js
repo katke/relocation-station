08270bc29d0a3fbcf24b2819062ba48c530c41b1
Kat <kmmpatke@gmail.com>
2018-12-30 17:27:41 -0800

    integrating google maps first pass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const gMapsKey = require("./config/env.json").mapsApiKey;
+const gMapsEndpoint = `https://maps.googleapis.com/maps/api/js?key=${gMapsKey}&callback=initMap&libraries=places`;
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      testing: "test!"
+    };
+  }
+  
+  componentDidMount = () => {
+    console.log("gMapsEndpoint: ", gMapsEndpoint);
+  }
+  
+  render = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+          <p>Welcome to Relocation Station</p>
+          <div id="map"></div>
       </div>
     );
   }
